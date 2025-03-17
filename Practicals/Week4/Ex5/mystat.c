01edd845253909d829dfbf70471ad8e0cce51fe4
Kevin Thomas <kt1238@york.ac.uk>
2025-03-17 17:57:30 -0400

    started Ex6

--- Practicals/Week4/Ex5/mystat.c
+++ Practicals/Week4/Ex5/mystat.c
@@ -2,9 +2,21 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
-int main(){
-	struct stat * buf;
-	printf("hello world!");
-	stat("test.txt", buf);
+int main(int argc, char *argv[]){
+
+	// say which file we are running mystat on
+	char intro[256] = "File: ";
+	strcat(strcat(intro, argv[1]),"\n");
+	printf(intro);
+	
+	struct stat fileinfo;
+	stat(argv[1], &fileinfo);
+	printf("Size: %d\n", fileinfo.st_size);
+	printf("Permissions: %o\n", fileinfo.st_mode);
+	printf("Blocks: %o\n", fileinfo.st_blocks);
+	printf("Links: %d\n", fileinfo.st_nlink);
+	return 0;
 }

